db4eca58dc13c6c820d568e595fc55b3e133e709
Lucasmdl21 <lucasmaiadaluz12345@gmail.com>
2025-12-03 19:54:04 -0300

    ATUALIZADO about me

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const button = document.querySelector('.header_nav_button');
 const mobileLinks = document.querySelector('.header_nav_links_mobile');
+const links = document.querySelectorAll('.header_nav_links_mobile a');
 
 button.addEventListener('click', () => {
     button.classList.toggle('open');
     mobileLinks.classList.toggle('active');
 });
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        button.classList.remove('open');
+        mobileLinks.classList.remove('active');
+    });
+});
+
+
+
+
